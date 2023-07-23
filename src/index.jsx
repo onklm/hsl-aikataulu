73ebc0f99eaf81c6c7e12dc229abb67f21f0ea0c
Onni Klemetti <onni.klemetti@gmail.com>
2023-07-23 18:02:28 +0300

    changed code

--- src/index.jsx
+++ src/index.jsx
@@ -1,35 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
+import { createRoot } from 'react-dom/client';
 import './style.css';
 
 function App() {
-  const [amountWater, setAmountWater, amountCoffee, setAmountCoffee] = React.useState();
+  const [amountWater, setAmountWater] = React.useState();
+  const [amountCoffee, setAmountCoffee] = React.useState();
+
   const calculateCoffeeAmount = () => {
-    return 0.06 * amountWater; 
+    return 0.06 * amountWater;
+    
+}
+  const changeAmountWater = (e) => {
+    setAmountWater(e.target.value);
+    setAmountCoffee(e.target.value * 0.06);
 
-  };
-
+  
+  }
+  
+    
+  const changeAmountCoffee = (e) => {
+    setAmountCoffee(e.target.value);
+    setAmountWater(e.target.value * 16.6667)
+  
+  }
   return (
     <div>
-      <p><label for="suhde">vahvuus</label>
-<select name="suhde" id="suhde" onclick="Calculate()" onchange="Calculate()"><option value="13">1:13
-</option><option value="14">1:14
-</option><option value="15">1:15
-</option><option value="16" selected="">1:16
-</option><option value="17">1:17</option>
-<option value="18">1:18
-</option><option value="19">1:19</option>
-</select></p>
-   
-      <label htmlFor="amountWater">Veden määrä</label>  
-      <input name="amountWater" value={amountWater} onChange={event => setAmountWater(event.target.value)} />ml  
+      <label htmlFor="amountWater">Veden määrä</label>
+      <input name="amountWater" value={amountWater} onChange={changeAmountWater} />ml
       <label htmlFor="amountCoffee">kahvin määrä</label>
-      <input name="amountCoffee" value ={amountCoffee} onchange={event => setAmountCoffee(event.target.value)} />g
-      
+      <input name="amountCoffee" value={amountCoffee} onChange={changeAmountCoffee} />g
     </div>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
+const root = createRoot(document.getElementById("root"));
+root.render(<App />,);
