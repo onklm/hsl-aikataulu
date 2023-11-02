ec3c880a076c69ea65b5fc059368fb80198a1f17
onklm <onni.klemetti@gmail.com>
2023-11-02 11:32:42 +0200

    changed code

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,31 @@ import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './styles/tailwind.css';
 
+function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timerID = setInterval(() => tick(), 1000);
+
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  }, []);
+
+  function tick() {
+    setTime(new Date());
+  }
+
+  return (
+    <div className="fixed top-0 right-0 m-4 p-4 bg-white text-black rounded shadow-lg text-4xl">
+      {time.toLocaleTimeString('fi-FI', { hour: '2-digit', minute: '2-digit' })}
+    </div>
+  );
+}
+
+
+
+
 function App() {
   const [schedule, setSchedule] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -52,43 +77,40 @@ function App() {
     date.setMinutes(date.getMinutes() + helsinkiOffset);
     return date.toISOString().substring(11, 16);
   }
-  
-  
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <div className="mt-10">Loading...</div>;
   }
 
   if (error) {
-    return <div>Error: {error}</div>;
+    return <div className="mt-10">Error: {error}</div>;
   }
 
   if (!schedule) {
-    return <div>No schedule available</div>;
+    return <div className="mt-10">No schedule available</div>;
   }
 
   return (
     <div className="min-h-screen bg-blue-800 text-white">
-      <div className="text-left p-4 ml-auto">
+      <div className="container mx-auto mr-0 text-left p-4">
         <div className="flex justify-between items-center mb-6">
-          <div className="text-6xl">
-        
-          </div>
+          {/* Lisätty Clock komponentti oikeaan yläkulmaan */}
+          <div className="text-6xl"></div>
+          <Clock />
         </div>
 
         <table className="w-full border-collapse mt-4">
           <thead>
-            <tr>
-            </tr>
+            <tr></tr>
           </thead>
           <tbody>
             {schedule.stoptimesWithoutPatterns.map((stoptime, index) => (
               <tr key={index}>
-                <td className="p-2 border-b border-white text-xl md:text-6xl text-left">{stoptime.trip.route.shortName}</td> {/* Lisätty text-left luokka tähän */}
-                <td className="p-2 border-b border-white text-xl md:text-4xl text-left">{stoptime.headsign}</td> {/* Lisätty text-left luokka tähän */}
-                <td className="p-2 border-b border-white text-xl md:text-8xl text-left">{/* Laituri/laiturit tiedot tähän */}</td> {/* Lisätty text-left luokka tähän */}
-                <td className="p-2 border-b border-white text-xl md:text-6xl text-left">
-                  <span className="pr-1">{formatTime(stoptime.realtimeDeparture)}</span>
+                <td className="p-3 border-b border-white text-xl md:text-6xl">{stoptime.trip.route.shortName}</td>
+                <td className="p-3 border-b border-white text-xl md:text-4xl">{stoptime.headsign}</td>
+                <td className="p-3 border-b border-white text-xl md:text-8xl"></td>
+                <td className="p-3 border-b border-white text-xl md:text-6xl">
+                  <span className="pr-10">{formatTime(stoptime.realtimeDeparture)}</span>
                 </td>
               </tr>
             ))}
