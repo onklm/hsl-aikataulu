6c79e27195dea8a85e4ca9235b6b43ff9f4b3c87
onklm <onni.klemetti@gmail.com>
2023-11-06 10:33:54 +0200

    added headers to table

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import './styles/tailwind.css';
 
-import {Clock} from './Clock';
+import { Clock } from './Clock';
 import { fetchSchedule } from './apiClient';
 
 
@@ -52,26 +52,29 @@ function App() {
   // Tämä osa on nyt korjattu ja sijoitettu oikein
   return (
     <div className="min-h-screen bg-blue-800 text-white flex flex-col">
-     <header className="p-0 flex justify-between items-center shadow-md">
-  <span className="text-xl font-bold"></span>
-  <div className=""> {/* Lisätty responsive fontti-koko */}
-    <Clock />
-  </div>
-</header>
+      <header className="p-0 flex justify-between items-center shadow-md">
+        <span className="text-xl font-bold"></span>
+        <div className=""> {/* Lisätty responsive fontti-koko */}
+          <Clock />
+        </div>
+      </header>
 
       <div className="flex-grow container mx-auto p-4">
         <table className="w-full border-collapse mt-4">
           <thead>
-            <tr></tr>
+            <tr>
+              <th className="p-2 border-b border-white text-2xl text-left">Linja</th>
+              <th className="p-2 border-b border-white text-2xl text-left">Määränpää</th>
+              <th className="p-2 border-b border-white text-2xl text-right">Aika / min</th>
+            </tr>
           </thead>
           <tbody>
             {schedule.stoptimesWithoutPatterns.map((stoptime, index) => (
               <tr key={index}>
-                <td className="p-3 border-b border-white text-xl md:text-6xl">{stoptime.trip.route.shortName}</td>
-                <td className="p-3 border-b border-white text-xl md:text-4xl">{stoptime.headsign}</td>
-                <td className="p-3 border-b border-white text-xl md:text-8xl"></td>
-                <td className="p-3 border-b border-white text-xl md:text-6xl">
-                  <span className="pr-10">{formatTime(stoptime.realtimeDeparture)}</span>
+                <td className="p-2 border-b border-white text-6xl">{stoptime.trip.route.shortName}</td>
+                <td className="p-2 border-b border-white text-4xl">{stoptime.headsign}</td>
+                <td className="p-2 border-b border-white text-6xl text-right">
+                  {formatTime(stoptime.realtimeDeparture)}
                 </td>
               </tr>
             ))}
