cef9ea2e7d550b095aab27fd2e42668736a11984
onklm <onni.klemetti@gmail.com>
2023-11-06 12:00:57 +0200

    changed clock and departure to update every 15 sec

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -2,23 +2,26 @@ import React, { useEffect, useState } from 'react';
 
 export function Clock() {
     const [time, setTime] = useState(new Date());
-  
+
     useEffect(() => {
-      const timerID = setInterval(() => tick(), 1000);
-  
-      return function cleanup() {
-        clearInterval(timerID);
-      };
+        const timerID = setInterval(() => tick(), 15000); // Asetetaan aikaväli 15 sekuntiin
+
+        return function cleanup() {
+            clearInterval(timerID);
+        };
     }, []);
-  
+
     function tick() {
-      setTime(new Date());
+        setTime(new Date());
     }
-  
+
     return (
-      <div className=" m-4 p-4 bg-white text-black rounded shadow-lg text-4xl">
-        {time.toLocaleTimeString('fi-FI', { hour: '2-digit', minute: '2-digit' })}
-      </div>
+        <div className="m-4 p-4 bg-white text-black rounded shadow-lg text-4xl">
+            {time.toLocaleTimeString('fi-FI', {
+                hour: '2-digit', 
+                minute: '2-digit', 
+                second: '2-digit' // Näytetään tunnit, minuutit ja sekunnit
+            })}
+        </div>
     );
-  }
-  
+}
