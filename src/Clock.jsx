3bf60f842b9f056b97cda6c8e9119fcb8147f57c
onklm <onni.klemetti@gmail.com>
2023-11-08 14:06:36 +0200

    changed to 2 digits and to update every second

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -4,8 +4,7 @@ export function Clock() {
     const [time, setTime] = useState(new Date());
 
     useEffect(() => {
-        const timerID = setInterval(() => tick(), 15000); // Asetetaan aikaväli 15 sekuntiin
-
+        const timerID = setInterval(() => tick(), 1000); // Asetetaan aikaväli 15 sekuntiin
         return function cleanup() {
             clearInterval(timerID);
         };
@@ -18,9 +17,8 @@ export function Clock() {
     return (
         <div className="m-4 p-4 bg-white text-black rounded shadow-lg text-4xl">
             {time.toLocaleTimeString('fi-FI', {
-                hour: '2-digit', 
-                minute: '2-digit', 
-                second: '2-digit' // Näytetään tunnit, minuutit ja sekunnit
+                hour: '2-digit',
+                minute: '2-digit',
             })}
         </div>
     );
