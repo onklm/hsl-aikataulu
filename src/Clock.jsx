6c55cebcd9de44d0e134d01e1bad8162c70080a8
Marko Klemetti <marko.klemetti@gmail.com>
2023-11-23 10:50:21 +0200

    feat: format clock

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,25 +1,25 @@
 import React, { useEffect, useState } from 'react';
 
 export function Clock() {
-    const [time, setTime] = useState(new Date());
+  const [time, setTime] = useState(new Date());
 
-    useEffect(() => {
-        const timerID = setInterval(() => tick(), 1000); // Asetetaan aikaväli 15 sekuntiin
-        return function cleanup() {
-            clearInterval(timerID);
-        };
-    }, []);
+  useEffect(() => {
+    const timerID = setInterval(() => tick(), 1000);
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  }, []);
 
-    function tick() {
-        setTime(new Date());
-    }
+  function tick() {
+    setTime(new Date());
+  }
 
-    return (
-        <div className="m-4 p-4 bg-white text-black rounded shadow-lg text-4xl">
-            {time.toLocaleTimeString('fi-FI', {
-                hour: '2-digit',
-                minute: '2-digit',
-            })}
-        </div>
-    );
+  return (
+    <div className="m-2 p-2 bg-white text-black rounded shadow-lg text-4xl">
+      {time.toLocaleTimeString('fi-FI', {
+        hour: '2-digit',
+        minute: '2-digit',
+      })}
+    </div>
+  );
 }
