944229ef2948b8e3c2c5ff871f2b35a00af88aa4
Marko Klemetti <marko.klemetti@gmail.com>
2023-11-23 10:50:32 +0200

    chore: remove comments

--- src/apiClient.js
+++ src/apiClient.js
@@ -15,7 +15,7 @@ export async function fetchSchedule(stopId, stopType = 'stop') {
         }
         headsign
       }
-    }  
+    }
   }`;
 
 
@@ -35,15 +35,12 @@ export async function fetchSchedule(stopId, stopType = 'stop') {
   }
 
   const data = await response.json();
-  
-  // Tarkistetaan, että saimme datan ja että se sisältää pysähtymisajat
+
   if (data.data && data.data[stopType] === 'station' && data.data[stopType].stoptimesWithoutPatterns) {
-    // Suodatetaan pois Kirkkonummen juna
     const filteredStoptimes = data.data[stopType].stoptimesWithoutPatterns.filter(
       stoptime => stoptime.headsign === 'Helsinki'
     );
 
-    // Asetetaan data.data[stopType].stoptimesWithoutPatterns olemaan suodatettu lista
     data.data[stopType].stoptimesWithoutPatterns = filteredStoptimes;
   }
 
