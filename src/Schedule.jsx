1d4a640bc04dbe7eceb6ef65d7970f41448edafb
Marko Klemetti <marko.klemetti@gmail.com>
2023-11-23 15:10:10 +0200

    fix: refine ui for loading view

--- src/Schedule.jsx
+++ src/Schedule.jsx
@@ -46,51 +46,42 @@ export function Schedule({ defaultStopId, stopType }) {
     };
   }, [stopId, stopType]);
 
-  const getMenuItemClass = (id) => {
-    return `px-2 sm:px-4 py-2 text-xs sm:text-sm font-semibold rounded-full ${id === stopId
-        ? 'bg-white text-blue-800'
-        : 'text-black hover:bg-gray-200 hover:text-blue-800'
-      }`;
-  };
-
-  if (loading) {
-    return <div className="mt-10">Loading...</div>;
-  }
-
   if (error) {
     return <div className="mt-10">Error: {error}</div>;
-  }
-
-  if (!schedule) {
-    return <div className="mt-10">No schedule available</div>;
   }
 
   return (
     <div className="min-h-screen bg-blue-800 text-white flex flex-col">
       <Header />
 
-      <div className="table-responsive">
-        <table className="w-full border-collapse mt-4">
-          <thead>
-            <tr>
-              <th className="p-2 border-b border-white text-2xl text-left">Linja</th>
-              <th className="p-2 border-b border-white text-2xl text-left">Määränpää</th>
-              <th className="p-2 border-b border-white text-2xl text-right">Aika / min</th>
-            </tr>
-          </thead>
-          <tbody>
-            {schedule.stoptimesWithoutPatterns.map((stoptime, index) => (
-              <tr key={index}>
-                <td className="p-2 border-b border-white text-4xl lg:text-6xl">{stoptime.trip.route.shortName}</td>
-                <td className="p-2 border-b border-white lg:text-4xl">{stoptime.headsign}</td>
-                <td className="p-2 border-b border-white text-4xl lg:text-6xl text-right">
-                  {getDepartureTime(stoptime)}
-                </td>
+      {loading ? (
+        <div className="flex justify-center items-center h-full mt-10">
+          <p className="text-4xl">Loading...</p>
+        </div>
+      ) : (
+        <div className="table-responsive">
+          <table className="w-full border-collapse mt-4">
+            <thead>
+              <tr>
+                <th className="p-2 border-b border-white text-2xl text-left">Linja</th>
+                <th className="p-2 border-b border-white text-2xl text-left">Määränpää</th>
+                <th className="p-2 border-b border-white text-2xl text-right">Aika / min</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {schedule.stoptimesWithoutPatterns.map((stoptime, index) => (
+                <tr key={index}>
+                  <td className="p-2 border-b border-white text-4xl lg:text-6xl">{stoptime.trip.route.shortName}</td>
+                  <td className="p-2 border-b border-white lg:text-4xl">{stoptime.headsign}</td>
+                  <td className="p-2 border-b border-white text-4xl lg:text-6xl text-right">
+                    {getDepartureTime(stoptime)}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 }
